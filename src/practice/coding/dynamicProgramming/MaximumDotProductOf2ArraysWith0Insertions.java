e0ea6d3aa28e3060d638c3ea5bcfa596aad298d1
rnuka <rnuka@twitter.com>
2017-01-24 08:45:14 -0800

    code changes

---
+++ src/practice/coding/dynamicProgramming/MaximumDotProductOf2ArraysWith0Insertions.java
@@ -0,0 +1,72 @@
+package practice.coding.dynamicProgramming;
+
+import java.util.Arrays;
+
+/*
+Question: Given two arrays of integers of size m and n where m > n. We need to maximize the dot product by
+inserting zeros in the second array but we cannot disturb the order of elements.
+
+Example-1:
+----------
+Input : A[] = {2, 3 , 1, 7, 8}
+        B[] = {3, 6, 7}
+Output : 107
+Explanation : We get maximum dot product after
+inserting 0 at first and third positions in
+second array.
+Maximum Dot Product : = A[i] * B[j]
+2*0 + 3*3 + 1*0 + 7*6 + 8*7 = 107
+
+
+Example-2:
+---------
+Input : A[] = {1, 2, 3, 6, 1, 4}
+        B[] = {4, 5, 1}
+Output : 46
+
+Link: http://www.geeksforgeeks.org/find-maximum-dot-product-two-arrays-insertion-0s/
+
+
+max_product(a,b,m,n) = max{ max_product(a,b,m-1,n-1)+(m*n),
+                              max_product(a,b,m-1,n)
+                             }
+                     = m*n when n=1
+
+ */
+public class MaximumDotProductOf2ArraysWith0Insertions {
+
+    public int maxDotProduct(int[] a, int [] b){
+        return maxDotProduct(a,b,a.length-1, b.length-1);
+    }
+
+    public int maxDotProduct(int[] a, int[] b, int m, int n){
+        if(m<0 || n<0){
+            return 1;
+        }
+        System.out.println("m="+m+" and n="+n+" product is::"+(a[m]*b[n]));
+        if(m==0 || n==0){
+            return a[m]*b[n];
+        }else{
+
+            return Math.max(maxDotProduct(a,b,m-1,n-1)+a[m]*b[n],
+                            maxDotProduct(a,b,m-1,n));
+        }
+    }
+
+    void testcase1(int[] a , int[] b){
+        System.out.println("maximum dot product of:: "+ Arrays.toString(a)+ " and "+Arrays.toString(b)
+                +" is::"+ maxDotProduct(a,b));
+    }
+
+    void testbed(){
+        testcase1(new int[]{2, 3 , 1, 7, 8}, new int[]{3, 6, 7});
+        //testcase1(new int[]{1, 2, 3, 6, 1, 4}, new int[]{4, 5, 1});
+    }
+
+    public static void main(String args[]){
+        MaximumDotProductOf2ArraysWith0Insertions m = new MaximumDotProductOf2ArraysWith0Insertions();
+        m.testbed();
+    }
+
+
+}
--- src/practice/coding/math/PrintPrimeFactors.java
+++ src/practice/coding/math/PrintPrimeFactors.java
@@ -10,5 +10,82 @@ Example-2: if the input number is 315, then output should be “3 3 5 7”.
 ----------
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrintPrimeFactors {
+
+    public List<Integer> primeFactors(int n){
+        List<Integer> results = new ArrayList<>();
+        primeFactors(n, results);
+        return results;
+    }
+
+    void primeFactors(int n, List<Integer> results){
+        if(n <= 1){
+            return;
+        }
+        int i;
+        for(i=2; i<n/2; i++){
+            if(n%i == 0 && isPrime(i)){
+                results.add(i);
+                break;
+            }
+        }
+        if(n%i == 0 && isPrime(n/i) && (n/i) != 1){
+            results.add(n/i);
+        }
+        primeFactors(n/i, results);
+
+    }
+
+    boolean isPrime(int k){
+        for(int i=2; i <= k-1; i++){
+            if(k%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    //Approach-2: Another readable approach
+    List<Integer> primeFactors2(int n){
+        List<Integer> results = new ArrayList<>();
+
+        int i = 2;
+
+        while(i <= n){
+            if( n%i == 0){
+                results.add(i);
+                n = n/i;
+            }else{
+                i++;
+            }
+        }
+        return results;
+    }
+
+    void testcase2(int n){
+        System.out.println("prime factors of n="+n+" are::"+primeFactors2(n));
+    }
+
+
+    void testcase1(int n){
+        System.out.println("prime factors of n="+n+" are::"+primeFactors(n));
+    }
+
+    void testbed(){
+        testcase2(12);
+        testcase2(315);
+        testcase2(100);
+        testcase1(10);
+    }
+
+    public static void main(String args[]){
+        PrintPrimeFactors p = new PrintPrimeFactors();
+        p.testbed();
+    }
+
+
 }
